484a21d7c7a6810f9ef86b0d9e29a0012a1ef5a0
Ruben Krbashyan <krbashyan.r@gmail.com>
2018-07-18 18:16:01 +0300

    project structure setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component, Fragment} from 'react'
+import { Header, Footer } from './Layouts'
+import Exercises from './Exercises'
+
+export default class extends Component {
+  render() {
+    return <Fragment>
+      <Header />
+        <Exercises/>
+      <Footer />
+    </Fragment>
+  }
+}
